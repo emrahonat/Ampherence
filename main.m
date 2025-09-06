91e588b1391c73c3faa267ee7fc3e941d4969e25
emrah onat <eonat87@yahoo.com>
2025-09-06 21:30:07 +0200

    first commit

---
+++ main.m
@@ -0,0 +1,8 @@
+%% MATLAB Code for Ampherence Map
+%
+% 1 - as an alternative guide map for path following based phase unwrapping algorithms
+% 2 - as an alternative map for change detection
+%
+% SAR Interferometry Applications
+%
+% Dr. Emrah Onat
